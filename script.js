90bb32dd22e0fb53195c8ef1b233d6cb4e2a53a1
adrianmihail <adrianmihail00@gmail.com>
2023-12-25 19:58:26 +0200

    Make square red after hovering on it
    The objective is to create trail behind, so a drawing can be done

--- script.js
+++ script.js
@@ -7,12 +7,19 @@ function createOneColumn() {
 
     for (let i = 0; i < numberOfSquares; i++) {
         squareArray[i] = document.createElement('div');
-        squareArray[i].id = 'square' + 1;
+        squareArray[i].id = 'square' + i;
+        squareArray[i].className = 'square';
+
         squareArray[i].setAttribute('style',
         'width:52px;height:52px;background-color:aqua;padding:0px;margin:0px');
-        
-        squareArray[i].className = 'square';
+
+        squareArray[i].addEventListener('mouseover', () => {
+            squareArray[i].setAttribute('style',
+        'width:52px;height:52px;background-color:red;padding:0px;margin:0px');
+        });
+
         grid.appendChild(squareArray[i]);
+        
     };
 };
 
